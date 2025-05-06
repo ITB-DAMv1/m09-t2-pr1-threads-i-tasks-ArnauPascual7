63cae6e0609ee7c44d91a598df82f78e34cd6619
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-05-06 19:57:33 +0200

    Created Practice Solution and Threads Project

---
+++ ThreadsTasksSolution/Part1-Threads/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part1_Threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
