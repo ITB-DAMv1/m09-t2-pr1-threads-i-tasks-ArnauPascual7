973fdd0fc16b8b90319d032c1cf8fcc2f15c5399
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-05-09 17:45:01 +0200

    Created Part2 Project

---
+++ ThreadsTasksSolution/Part2-Tasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part2_Tasks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
