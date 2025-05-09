6e025af8eb30bf84801e815c9f978ac53b39ceeb
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-05-09 21:39:24 +0200

    Created Spaceship and Position Classes

---
+++ ThreadsTasksSolution/Part2-Tasks/Models/Spaceship.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2_Tasks.Models
+{
+    public class Spaceship
+    {
+        public Position Position { get; set; }
+    }
+}
